45a6ad03aa98f730cc573f2e89c10aae48e67704
Brainer Esteban Betancur Gonzalez <brainer.betancur@masglobalconsulting.com>
2021-11-23 13:34:00 -0500

    Initial Commit

---
+++ SocialMedia.API/Controllers/PostController.cs
@@ -0,0 +1,23 @@
+﻿ using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Infraestructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult Index()
+        {
+            var posts = new PostRepository().GetPosts();
+            return Ok (posts)  ;
+        }
+    }
+}
---
+++ SocialMedia.Core/Interfaces/IPostRepository.cs
@@ -0,0 +1,12 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialMedia.Core.Interfaces
+{
+    interface IPostRepository
+    {
+        public IEnumerable<Post> GetAllPost();
+    }
+}
